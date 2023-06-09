8c5cf4db21d8f0f6981c26056ea4dd3bea1614cb
endorf8n <128092594+endorf8n@users.noreply.github.com>
2023-06-10 00:22:20 +0300

    add test function

--- src/js/main-page.js
+++ src/js/main-page.js
@@ -1,29 +1,40 @@
 import { SwaggerAPI } from './swagger-api';
 
-const bookContainer = document.querySelector('.category-list');
+const booksContainer = document.querySelector('.category-list');
 
 const topBooksAPI = new SwaggerAPI();
 
-topBooksAPI.fetchTopBooks().then(response => console.log(response.data));
+const paintBestSellerBooks = async () => {
+  try {
+    const { data } = await topBooksAPI.fetchTopBooks();
+    console.log(data);
+    booksContainer.innerHTML = createBookCards(data);
+  } catch (err) {
+    console.log(err);
+  }
+};
 
-// function createBookCards(bookInfo) {
-//   const booksMarkup = booksInfo
-//     .map(book => {
-//       const {} = book;
-//       return `<li class="category-list-item">
-//                 <a class="book-link" href="">
-//                   <div class="book-thumb">
-//                     <img class="book-image" src="" alt="" />
-//                     <p class="book-overlay"></p>
-//                   </div>
-//                   <div class="book-card-content">
-//                     <h2 class="book-title"></h2>
-//                     <p class="book-author"></p>
-//                   </div>
-//                 </a>
-//             </li>;`;
-//     })
-//     .join('');
+paintBestSellerBooks();
 
-//   return booksMarkup;
-// }
+function createBookCards(bookInfo) {
+  const booksMarkup = bookInfo
+    .map(book => {
+      const info = book.books[0];
+
+      return `<li class="category-list-item">
+                <a class="book-link" href="">
+                  <div class="book-thumb">
+                    <img class="book-image" src="${info.book_image}" alt="${info.description}" />
+                    <p class="book-overlay"></p>
+                  </div>
+                  <div class="book-card-content">
+                    <h2 class="book-title">${info.title}</h2>
+                    <p class="book-author">${info.author}</p>
+                  </div>
+                </a>
+            </li>`;
+    })
+    .join('');
+
+  return booksMarkup;
+}
