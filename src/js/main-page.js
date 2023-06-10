395eb30d72498a7ca4b1910e7151efe3223dde80
Vyachuk <Slavkoka045@gmail.com>
2023-06-10 11:53:22 +0300

    update main html

--- src/js/main-page.js
+++ src/js/main-page.js
@@ -2,49 +2,19 @@ import { SwaggerAPI } from './swagger-api';
 
 const booksContainer = document.querySelector('.category-list');
 
-const topBooksAPI = new SwaggerAPI();
+let bodyWidth = 0;
 
-const paintBestSellerBooks = async () => {
-  try {
-    const { data } = await topBooksAPI.fetchTopBooks();
-
-    for (let i = 0; i < 1; i++) {
-      let topBooks = data[i].books;
-      console.log(topBooks);
-
-      booksContainer.innerHTML = createBook(topBooks);
-    }
-    // booksContainer.innerHTML = createBookCards(topBooks);
-  } catch (err) {
-    console.log(err);
-  }
+onload = event => {
+  bodyWidth = event.target.body.clientWidth;
 };
 
-paintBestSellerBooks();
-
-// function createBook(bookInfo) {
-//   const markup = bookInfo
-//     .map(book => {
-//       const { book_image, description, title, author } = book;
-//       return `<li class="category-list-item">
-//                        <a class="book-link" href="">
-//                           <img class="book-image" src="${book_image}" alt="${description}" />
-//                            <div class="book-card-content">
-//                             <h2 class="book-title">${title}</h2>
-//                            <p class="book-author">${author}</p>
-//                          </div>
-//                       </a>
-//                      </li>`;
-//     })
-//     .join('');
-//   return markup;
-// }
+const topBooksAPI = new SwaggerAPI();
 
 function createBook(bookInfo) {
   const markup = bookInfo
     .map(book => {
       const { book_image, description, title, author } = book;
-      return `
+      return `<li class="category-list-item">
                        <a class="book-link" href="">
                           <img class="book-image" src="${book_image}" alt="${description}" />
                            <div class="book-card-content">
@@ -52,38 +22,40 @@ function createBook(bookInfo) {
                            <p class="book-author">${author}</p>
                          </div>
                       </a>
-                     `;
+                     </li>`;
     })
     .join('');
   return markup;
 }
-let bodyWidth = 0;
 
-onload = event => {
-  bodyWidth = event.target.body.clientWidth;
-};
 
 async function createBlock() {
   const { data } = await topBooksAPI.fetchTopBooks();
-  console.log(data);
-
-  if (bodyWidth >= 768) {
-    console.log(5);
-    return;
-  }
-  if (bodyWidth >= 375) {
-    console.log(3);
-    return;
-  }
+  
   for (let i = 0; i < 4; i++) {
     let topBooks = data[i].books;
-    booksContainer.innerHTML = `<p class="category-name">${data[0].list_name}</p>
-    <li>${createBook}</li>
-    <button class="category-list-button">see more</button>`;
-    console.log(topBooks);
-  }
+    let markup = null;
+    let markUpCount = 0;
 
-  console.log(1);
+    if (bodyWidth <= 375) {
+      markUpCount = 1;
+    }
+    if (bodyWidth > 375 && bodyWidth < 768) {
+      markUpCount = 3;
+    }
+    if (bodyWidth >= 768) {
+      markUpCount = 5;
+    }
+    markup = `<li>
+        <p class="category-name">${topBooks[0].list_name}</p>
+        <ul>
+          ${createBook(topBooks.slice(0, markUpCount))}
+        </ul>
+       <button class="category-list-button">see more</button>
+      </li>`;
+
+    booksContainer.insertAdjacentHTML('beforeend', markup);
+  }
 }
 
 createBlock();
