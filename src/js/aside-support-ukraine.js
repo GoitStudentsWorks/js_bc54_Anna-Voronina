abc4d08b319475532c2f2e470aaea506f2175d82
Valeriy94-ua <128257210+Valeriy94-ua@users.noreply.github.com>
2023-06-11 16:24:04 +0300

    add-fonds-js

--- src/js/aside-support-ukraine.js
+++ src/js/aside-support-ukraine.js
@@ -1,7 +1,77 @@
-document
-  .querySelector('.sup-ukr-scroll-btn')
-  .addEventListener('click', function () {
-    const modalContent = document.querySelector('.sup-content-wrp');
-    modalContent.scrollTop = modalContent.scrollHeight;
-  });
-console.log(111);
+import { supUkrFonds } from './aside-support-fonds';
+
+// -----------VERTICAL SCROLL---------------
+const supUkrBtn = document.querySelector('.sup-ukr-scroll-btn');
+const modalContent = document.querySelector('.sup-content-wrp');
+
+const supUkrScroll = () => {
+  modalContent.scrollTop = modalContent.scrollHeight;
+};
+
+supUkrBtn.addEventListener('click', supUkrScroll);
+
+// ------------RANDERING FONDS---------------
+
+const supportCompaniesContainer = document.querySelector('.sup-content-wrp');
+
+const supportCompanyElements = supUkrFonds.map((fund, index) => {
+  const supportCompany = document.createElement('div');
+  supportCompany.className = 'support-company';
+
+  const supCompNumb = document.createElement('p');
+  supCompNumb.className = 'sup-comp-numb';
+  supCompNumb.textContent = (index + 1).toString().padStart(2, '0');
+
+  supportCompany.appendChild(supCompNumb);
+
+  if (fund.title && fund.url) {
+    const link = document.createElement('a');
+    link.href = fund.url;
+    link.target = '_blank';
+    link.rel = 'noopener noreferrer';
+
+    if (fund.img) {
+      const image = document.createElement('img');
+      image.className = 'sup-company-img';
+      image.src = fund.img;
+      image.alt = fund.title;
+
+      link.appendChild(image);
+    }
+
+    supportCompany.appendChild(link);
+  }
+
+  return supportCompany;
+});
+
+supportCompanyElements.forEach(element => {
+  supportCompaniesContainer.appendChild(element);
+});
+
+//       image.style.height = '32px';
+// else {
+//       link.textContent = fund.title;
+//     }
+
+// -------------------old var--------------------------
+
+// const makeFondsList = ({ title, url, img }) => {
+//   return `<a href="${url}">
+//             <img
+//               class="sup-company"
+//               style="height: 32px"
+//               src="${img}"
+//               alt="${title}"
+//             />
+//           </a>`;
+// };
+
+// const makeFondsArr = supUkrFonds.map(fondData => {
+//   return makeFondsList(fondData);
+// });
+
+// <div class="support-company">
+//   <p class="sup-comp-numb">06</p>
+//   <!-- <a href=""><img style="height: 32px" src="" alt="" /></a> -->
+// </div>
