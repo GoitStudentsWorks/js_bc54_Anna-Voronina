1e57abf608b2a2155984f4fd31bf8acfba5f434e
Vlad0si4 <boiko.vlad91@gmail.com>
2023-06-11 17:11:28 +0300

    add

--- src/js/aside-categories.js
+++ src/js/aside-categories.js
@@ -2,42 +2,48 @@ import { SwaggerAPI } from './swagger-api';
 import createCategoriesMarkup from './templates/create-categories';
 
 const categoriesList = document.querySelector('.aside-list');
+const categoriesTitle = document.querySelector('.aside-title')
 const renderingCategories = document.querySelector('.main-content');
-console.log(categoriesList);
-
-
 
 const swaggerCategoriesApi = new SwaggerAPI();
 
-swaggerCategoriesApi
-  .fetchBooksCategoryList()
-  .then(({ data }) => {
-   
-
-    const searchCategory = data
-      .map(category => {
-        return `<a class="aside-link"><li class="aside-item">${category.list_name}</li></a>`;
-      })
-      .join('');
+const createCategoriesItem = async () => {
+  try {
+    const { data } = await swaggerCategoriesApi.fetchBooksCategoryList();
+    const searchCategory = data.map(category => {
+      return `<a class="aside-link"><li class="aside-item">${category.list_name}</li></a>`;
+    }).join('');
     categoriesList.innerHTML = searchCategory;
-     
-  })
-  .catch(error => {
+  } catch (error) {
     console.log(error);
-  });
+  };
+};
+createCategoriesItem();
+
+
+let activeCategoryLink = null;
+
 
 //ф-я відображення книг
 const onCategoriesLinkClick = event => {
   const value = event.target.textContent;
   swaggerCategoriesApi.categoryName = value;
   
+   if (activeCategoryLink) {
+    activeCategoryLink.classList.remove('active'); 
+  };
+  
+  activeCategoryLink = event.target
+  activeCategoryLink.classList.add('active'); 
+  categoriesTitle.classList.remove('active');
 
-  swaggerCategoriesApi
-    .fetchBooksByCategory(value)
-    .then(({ data }) => {
-   
+  const createMarkupCategories = async () => {
+    try {
+      const { data } = await swaggerCategoriesApi.fetchBooksByCategory(value);
+      console.log(data);
+
       const listName = data[0].list_name;
-
+  
       const words = listName.split(' ');
       const halfIndex = words.length / 2;
 
@@ -49,10 +55,10 @@ const onCategoriesLinkClick = event => {
         data
       )}</ul>`;
 
-    })
-    .catch(error => {
+    } catch (error) {
       console.log(error);
-    });
-};
-
+    };
+  };
+  createMarkupCategories()
+}
 categoriesList.addEventListener('click', onCategoriesLinkClick);
