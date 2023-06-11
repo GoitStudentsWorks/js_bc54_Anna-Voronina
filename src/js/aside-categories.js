bf0eeab746f5bb166c68228427b7f84a9c6ec43e
Vlad0si4 <boiko.vlad91@gmail.com>
2023-06-11 12:57:53 +0300

    add link on all categories

--- src/js/aside-categories.js
+++ src/js/aside-categories.js
@@ -3,19 +3,24 @@ import createCategoriesMarkup from './templates/create-categories';
 
 const categoriesList = document.querySelector('.aside-list');
 const renderingCategories = document.querySelector('.main-content');
-console.log(renderingCategories);
+console.log(categoriesList);
+
+
 
 const swaggerCategoriesApi = new SwaggerAPI();
 
 swaggerCategoriesApi
   .fetchBooksCategoryList()
   .then(({ data }) => {
+   
+
     const searchCategory = data
       .map(category => {
         return `<a class="aside-link"><li class="aside-item">${category.list_name}</li></a>`;
       })
       .join('');
     categoriesList.innerHTML = searchCategory;
+     
   })
   .catch(error => {
     console.log(error);
@@ -25,10 +30,12 @@ swaggerCategoriesApi
 const onCategoriesLinkClick = event => {
   const value = event.target.textContent;
   swaggerCategoriesApi.categoryName = value;
+  
 
   swaggerCategoriesApi
     .fetchBooksByCategory(value)
     .then(({ data }) => {
+   
       const listName = data[0].list_name;
 
       const words = listName.split(' ');
@@ -41,6 +48,7 @@ const onCategoriesLinkClick = event => {
       renderingCategories.innerHTML = `<h1 class="categories-title">${decoratedListName}</h1> <ul class="categories-item">${createCategoriesMarkup(
         data
       )}</ul>`;
+
     })
     .catch(error => {
       console.log(error);
