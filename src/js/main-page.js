8e22f013a376fcc37a3f91b3f65d574dc61f789a
endorf8n <128092594+endorf8n@users.noreply.github.com>
2023-06-12 18:13:14 +0300

    function empty

--- src/js/main-page.js
+++ src/js/main-page.js
@@ -1,4 +1,14 @@
+import book322_1 from '../images/shopping/books_322_@1.webp';
+import book322_2 from '../images/shopping/books_322_@2.webp';
+import book322_11 from '../images/shopping/books_322_@1.png';
+import book322_22 from '../images/shopping/books_322_@2.png';
+import book265_1 from '../images/shopping/books_265_@1.webp';
+import book265_2 from '../images/shopping/books_265_@2.webp';
+import book265_11 from '../images/shopping/books_265_@1.png';
+import book265_22 from '../images/shopping/books_265_@2.png';
+
 import { SwaggerAPI } from './swagger-api.js';
+import { Notify } from 'notiflix';
 import createBook from './templates/create-book.js';
 import addListener from './modal-window.js';
 
@@ -33,6 +43,11 @@ async function createBlock() {
         markUpCount = 5;
       }
 
+      if (topBooks.length === 0) {
+        Notify.failure('There are no best sellers books in this category');
+        return;
+      }
+
       const markup = `<li class="category-list-item top-list-item">
         <p class="category-name" data-category-name="${
           topBooks[0].list_name
@@ -47,9 +62,9 @@ async function createBlock() {
       findBtn();
     }
   } catch (error) {
-    console.log(error);
+    Notify.failure('Something went wrong. Please, try later.');
   }
-  addListener()
+  addListener();
 }
 
 function findBtn() {
@@ -68,25 +83,28 @@ async function onSeeMoreBtnClick(event) {
     topBooksAPI.categoryName = name;
     const { data } = await topBooksAPI.fetchBooksByCategory();
 
+    if (data.length === 0) {
+      booksContainer.innerHTML = emptySeeMoreBooksMarkup();
+      return;
+    }
+
     title.innerHTML = divideTitleElements(name);
     booksContainer.classList.add('category-list-click');
     booksContainer.innerHTML = createBook(data);
     addActiveClassToCategoryListItem(name);
+    addListener();
   } catch (error) {
-    console.log(error);
+    Notify.failure('Something went wrong. Please, try later.');
   }
 }
 
 function divideTitleElements(categoryName) {
-
-
   const words = categoryName.split(' ');
   const lastWord = words[words.length - 1];
   const otherWords = words.slice(0, words.length - 1).join(' ');
 
   return `<span class="home-title-decor">${otherWords} </span>${lastWord}`;
 }
-
 
 function addActiveClassToCategoryListItem(name) {
   const asideCategoryItems = document.querySelectorAll('.aside-item');
@@ -98,4 +116,31 @@ function addActiveClassToCategoryListItem(name) {
       asideCategoryItem.classList.toggle('active');
     }
   });
+}
+
+function emptySeeMoreBooksMarkup() {
+  return `
+    <p class="shopping-list-text">Sorry. Could not find books of this category.</p>
+        <picture>
+            <source media="(min-width: 768px)" srcset="
+                            ${book322_1} 1x,
+                            ${book322_2} 2x
+                            " type="image/webp" />
+            <source media="(min-width: 768px)" srcset="
+                            ${book322_11} 1x,
+                            ${book322_22} 2x
+                            " type="image/png" />
+
+            <source media="(max-width: 767px)" srcset="
+                            ${book265_1} 1x,
+                            ${book265_2} 2x
+                            " type="image/webp" />
+            <source media="(max-width: 767px)" srcset="
+                            ${book265_11} 1x,
+                            ${book265_22} 2x
+                            " type="image/png" />
+
+            <img class="shopping-list-image" src="${book265_1}" alt="Books" loading="lazy" />
+        </picture>
+    `;
 }
