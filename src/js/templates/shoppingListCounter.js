15ebc3eca007aa628c449138d3c3d0c26f92c90b
Anna-Voronina <anna.vs2512@gmail.com>
2023-06-12 16:03:35 +0300

    Update

--- src/js/templates/shoppingListCounter.js
+++ src/js/templates/shoppingListCounter.js
@@ -1,12 +1,12 @@
 import Storage from '../local-storage';
-const shoppingCountEl = document.querySelector('.header-shopping-count')
+const shoppingCountEl = document.querySelector('.header-shopping-count');
 
-export default countBooks = () => {
-    const storageBook = Storage.load('bookList');
-    if (storageBook.length > 0) {
-        shoppingCountEl.classList.remove('is-hidden');
-        shoppingCountEl.textContent = storageBook.length;
-    } else {
-        shoppingCountEl.classList.add('is-hidden');
-    }
-}
+export const countBook = () => {
+  const storageBook = Storage.load('bookList');
+  if (storageBook.length > 0) {
+    shoppingCountEl.classList.remove('is-hidden');
+    shoppingCountEl.textContent = storageBook.length;
+  } else {
+    shoppingCountEl.classList.add('is-hidden');
+  }
+};
