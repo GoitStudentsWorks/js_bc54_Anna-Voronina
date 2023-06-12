23b9f110fec38b66ad2eb664dee906c7b0891901
Vlad0si4 <boiko.vlad91@gmail.com>
2023-06-12 10:45:20 +0300

    add cursor pointer

--- src/js/aside-categories.js
+++ src/js/aside-categories.js
@@ -25,16 +25,54 @@ let activeCategoryLink = null;
 
 
 //ф-я відображення книг
+// const onCategoriesLinkClick = event => {
+//   const value = event.target.textContent;
+//   swaggerCategoriesApi.categoryName = value;
+  
+//    if (activeCategoryLink) {
+//     activeCategoryLink.classList.remove('active'); 
+//   };
+  
+//   activeCategoryLink = event.target
+//   activeCategoryLink.classList.add('active'); 
+//   categoriesTitle.classList.remove('active');
+
+//   const createMarkupCategories = async () => {
+//     try {
+//       const { data } = await swaggerCategoriesApi.fetchBooksByCategory(value);
+//       console.log(data);
+
+//       const listName = data[0].list_name;
+  
+//       const words = listName.split(' ');
+//       const halfIndex = words.length / 2;
+
+//       const firstHalf = words.slice(0, halfIndex).join(' ');
+//       const secondHalf = words.slice(halfIndex).join(' ');
+
+//       const decoratedListName = `${firstHalf} <span class="categories-title-decor">${secondHalf}</span>`;
+//       renderingCategories.innerHTML = `<h1 class="categories-title">${decoratedListName}</h1> <ul class="categories-item">${createCategoriesMarkup(
+//         data
+//       )}</ul>`;
+
+//     } catch (error) {
+//       console.log(error);
+//     };
+//   };
+//   createMarkupCategories()
+// }
+// categoriesList.addEventListener('click', onCategoriesLinkClick);
+
 const onCategoriesLinkClick = event => {
   const value = event.target.textContent;
   swaggerCategoriesApi.categoryName = value;
-  
-   if (activeCategoryLink) {
-    activeCategoryLink.classList.remove('active'); 
-  };
-  
-  activeCategoryLink = event.target
-  activeCategoryLink.classList.add('active'); 
+
+  if (activeCategoryLink) {
+    activeCategoryLink.classList.remove('active');
+  }
+
+  activeCategoryLink = event.target;
+  activeCategoryLink.classList.add('active');
   categoriesTitle.classList.remove('active');
 
   const createMarkupCategories = async () => {
@@ -43,22 +81,21 @@ const onCategoriesLinkClick = event => {
       console.log(data);
 
       const listName = data[0].list_name;
-  
+
       const words = listName.split(' ');
-      const halfIndex = words.length / 2;
+      const lastWord = words[words.length - 1];
+      const otherWords = words.slice(0, words.length - 1).join(' ');
 
-      const firstHalf = words.slice(0, halfIndex).join(' ');
-      const secondHalf = words.slice(halfIndex).join(' ');
-
-      const decoratedListName = `${firstHalf} <span class="categories-title-decor">${secondHalf}</span>`;
+      const decoratedListName = `${otherWords} <span class="categories-title-decor">${lastWord}</span>`;
       renderingCategories.innerHTML = `<h1 class="categories-title">${decoratedListName}</h1> <ul class="categories-item">${createCategoriesMarkup(
         data
       )}</ul>`;
-
     } catch (error) {
       console.log(error);
-    };
+    }
   };
-  createMarkupCategories()
-}
+
+  createMarkupCategories();
+};
+
 categoriesList.addEventListener('click', onCategoriesLinkClick);
