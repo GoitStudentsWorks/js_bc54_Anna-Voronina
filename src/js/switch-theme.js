d142a0a6800769df2e9b475bda8a0cbaf904eb8a
Eleonora907 <elenatroyan2010@ukr.net>
2023-06-12 11:15:15 +0200

    .

--- src/js/switch-theme.js
+++ src/js/switch-theme.js
@@ -1,6 +1,7 @@
 const switchInput = document.querySelector('.switch input');
 
 switchInput.addEventListener('change', () => {
+  // preventDefault();
   // Проверяем состояние переключателя
   if (switchInput.checked) {
     // Применяем стили для темной темы
