b0b5c5db81f60f59418f6be8151b78e059150d34
Eleonora907 <elenatroyan2010@ukr.net>
2023-06-12 15:12:15 +0200

    .

--- src/js/burger-menu.js
+++ src/js/burger-menu.js
@@ -1,20 +1,27 @@
-   const modal = document.getElementById("burger-modal");
-   const openButton = document.querySelector(".js-open-menu-hamburger");
-   const closeButton = document.querySelector(".burger-modal-btn");
-   
-   openButton.addEventListener("click", openModal);
-   closeButton.addEventListener("click", closeModal);
-   
-   function openModal() {
-     modal.style.display = "block";
-     openButton.classList.add("hidden"); 
-   }
-   
-   function closeModal() {
-     modal.style.display = "none";
-     openButton.classList.remove("hidden"); 
-   }
-   
-   window.addEventListener("DOMContentLoaded", function() {
-     modal.style.display = "none";
-   });
+const modal = document.getElementById("mobile-menu-modal");
+const openButton = document.querySelector(".js-open-menu-burger");
+const closeButton = document.querySelector(".close-mobile-modal-btn");
+
+openButton.addEventListener("click", openModal);
+closeButton.addEventListener("click", closeModal);
+
+function openModal() {
+  modal.style.display = "block";
+  openButton.classList.add("hidden");
+  closeButton.classList.remove("hidden");
+  document.body.classList.add("modal-open");
+}
+
+function closeModal() {
+  modal.style.display = "none";
+  openButton.classList.remove("hidden");
+  closeButton.classList.add("hidden");
+  document.body.classList.remove("modal-open");
+}
+
+window.addEventListener("DOMContentLoaded", function() {
+  modal.style.display = "none";
+  closeButton.classList.add("hidden");
+});
+
+
