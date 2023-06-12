483994411905b4f3717c647b6267c71fe11e5962
Eleonora907 <elenatroyan2010@ukr.net>
2023-06-12 15:27:39 +0200

    .

--- src/js/index.js
+++ src/js/index.js
@@ -1,6 +1,6 @@
 import './aside-categories.js';
 import './aside-support-ukraine.js';
-// import './burger-menu.js';
+import './burger-menu.js';
 import './header.js';
 import './main-page.js';
 import './modal-window.js';
